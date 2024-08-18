68f1590766b078cac223d6a80534728530039ed2
mjndfreakk <cagdasergencc@gmail.com>
2024-08-18 16:19:40 +0300

    Initial commit

---
+++ ArrayExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Threading.Channels;
+
+int[] numbers = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    numbers[i] = i + 1;
+}
+
+foreach (var number in numbers)
+{
+    Console.Write(number + ", ");
+}
+
+int[] numbers2 = new int[11];
+
+for (int i = 0; i < 10; i++)
+{
+    numbers2[i] = numbers[i];
+}
+
+
+numbers2[10] = Convert.ToInt32(Console.ReadLine());
+Array.Sort(numbers2);
+
+for (int i = 10; i > -1; i--)
+{
+    if (i == 0)
+        Console.Write(numbers2[i]);
+    else
+    {
+        Console.Write(numbers2[i] + ", ");
+    }
+}
+
